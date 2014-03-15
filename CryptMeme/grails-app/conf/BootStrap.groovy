91a74abcfbb2a482a1f95632444413d6423afdfd
NoYouShutup <github@sonicpoets.com>
2014-03-15 19:29:44 -0400

    Project settings are all sorts of wack, but at least I've got the I2P Client Service working now.

--- CryptMeme/grails-app/conf/BootStrap.groovy
+++ CryptMeme/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,15 @@
-class BootStrap {
+import net.i2p.router.Router
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+class BootStrap {
+	def I2PClientService
+	
+	public static Router router;
+	
+	def init = {
+		println "In BootStrap. Initializing I2P...\n";
+		I2PClientService.init();
+	}
+	def destroy = {
+		router.shutdownGracefully();
+	}
 }
